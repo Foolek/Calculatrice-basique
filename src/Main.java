30a553a188426ae725af80e755a1e832640ecaf2
Foolek <80790342+Foolek@users.noreply.github.com>
2022-05-31 23:36:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+		run();
+	}
+
+	private static void run(){
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Fenetre().setVisible(true);
+			}
+		});
+	}
+
+}
